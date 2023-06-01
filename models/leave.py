7646b6aa7563a0e7c32f7710802be84ce120c5ce
Sébastien Bühl <buhlsebastien@gmail.com>
2023-06-01 07:45:38 +0200

    [FIX] use the Salary name and not the tag

--- models/leave.py
+++ models/leave.py
@@ -5,4 +5,5 @@ from odoo import fields, models
 class HolidaysRequest(models.Model):
     _inherit = 'hr.leave'
     
-    struc_type = fields.Char(related="employee_id.category_ids.display_name", string='Salary Structure')
+    struc_type = fields.Char(related="employee_id.contract_id.structure_type_id.name", string='Salary Structure', store=True)
+
