35f015478f9e1b21582ed3f4c1d79691e7ba1959
Sébastien Bühl <buhlsebastien@gmail.com>
2023-06-21 09:21:21 +0200

    [ADD] Work location on Time Off and Allocations

--- models/leave.py
+++ models/leave.py
@@ -6,4 +6,10 @@ class HolidaysRequest(models.Model):
     _inherit = 'hr.leave'
     
     struc_type = fields.Char(related="employee_id.contract_id.structure_type_id.name", string='Salary Structure', store=True)
+    work_location = fields.Char(related="employee_id.work_location_id.display_name", string='Work Location', store=True)
 
+class HolidaysAllocation(models.Model):
+    _inherit = "hr.leave.allocation"
+    
+    struc_type = fields.Char(related="employee_id.contract_id.structure_type_id.name", string='Salary Structure', store=True)
+    work_location = fields.Char(related="employee_id.work_location_id.display_name", string='Work Location', store=True)
