cbab7edce75c98ace2fc2cd9c382f5e097b6fe1a
Sébastien Bühl <buhlsebastien@gmail.com>
2024-07-23 07:20:04 +0200

    [17.0][MIG] wip

--- __manifest__.py
+++ __manifest__.py
@@ -12,7 +12,7 @@
     'website': "http://www.buhl.be",
 
     'category': 'Customizations',
-    'version': '0.1.8.6',
+    'version': '17.0.0.0',
     'license': 'LGPL-3',
 
     # any module necessary for this one to work correctly
